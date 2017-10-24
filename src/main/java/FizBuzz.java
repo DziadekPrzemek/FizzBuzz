21e03286c66a4d8edbab9f11ae649dc4e3ffcca4
Dziadek <dziadek.przemek@gmail.com>
2017-10-24 17:25:41 +0200

    Add comments and condition while changed

--- src/main/java/FizBuzz.java
+++ src/main/java/FizBuzz.java
@@ -6,16 +6,16 @@ public class FizBuzz {
 	public static void main(String[] args) {
 		
 		System.out.println("Set last number: ");
-		in = new Scanner(System.in);
+		in = new Scanner(System.in);				//Read keybord
 		int i = in.nextInt();
 		int y = 1;
 		
-		while(y < i) {
-		if(y%3==0) {
+		while(y <= i) {								//Count loops
+		if(y%3==0) {								// Condition of Fizz
 			System.out.println("Fizz");
 		}
 		else if(y%5==0) {
-			System.out.println("Buzz");
+			System.out.println("Buzz");				//Condition of Buzz
 		}
 		else 
 		{
